a974990ec76077c79c3327abb340c3f40235bc40
Sasha <komarolexandr@gmail.com>
2020-10-27 21:13:31 +0200

    update Calculator

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		String operator = "^";
 		int first = 11;
-		int second = 8;
+		int second = 3;
 		if (operator == "+") {
 			int sum = first + second;
 			System.out.println("Sum = " + sum);
@@ -16,8 +16,8 @@ public class Calculator {
 			int div = first / second;
 			System.out.println("Div = " + div);
 		} else if (operator == "^") {
-			int sq = first;
-			for (int i = 1; i < second; i++) {
+			int sq = 1;
+			for (int i = 1; i <= second; i++) {
 				sq *= first;
 			}
 			System.out.println("Sq = " + sq);
