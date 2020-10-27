a012e3295dc69abba1b7cd569023924a6067cfa4
Sasha <komarolexandr@gmail.com>
2020-10-27 20:33:28 +0200

    three new tasks

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+	public static void main(String[] args) {
+		String operator = "^";
+		int first = 11;
+		int second = 8;
+		if (operator == "+") {
+			int sum = first + second;
+			System.out.println("Sum = " + sum);
+		} else if (operator == "-") {
+			int mul = first - second;
+			System.out.println("Mul = " + mul);
+		} else if (operator == "*") {
+			int mut = first * second;
+			System.out.println("Mut = " + mut);
+		} else if (operator == "/") {
+			int div = first / second;
+			System.out.println("Div = " + div);
+		} else if (operator == "^") {
+			int sq = first;
+			for (int i = 1; i < second; i++) {
+				sq *= first;
+			}
+			System.out.println("Sq = " + sq);
+		} else if (operator == "%") {
+			int mod = first % second;
+			System.out.println("Mod = " + mod);
+		}
+	}
+}
---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,16 @@
+public class MyFirstGame {
+	public static void main(String[] args) {
+		int number = 10;
+		int result = 50;
+		while (result != number) {
+			if (result > number) {
+				System.out.println("Result less number");
+				result = result / 2;
+			} else if (result < number) {
+				System.out.println("Result more number");
+				result = result + 1;
+			}
+		}
+		System.out.println("You win!");
+	}
+}
